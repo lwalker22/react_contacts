a4028a66b68fdf64dd7f41562566432979a84680
Logan Walker <loganwalker22@gmail.com>
2020-06-16 20:02:50 -0600

    created form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 import Contacts from './components/contacts/Contacts';
+import ContactForm from './components/contacts/ContactForm';
 class App extends Component {
   state = { contacts: [
     { id: 1, firstName: 'John', phone: '123-123-1233'},
     { id: 2, firstName: 'Sally', phone: '123-333-1233'},
     { id: 3, firstName: 'Alex', phone: '123-125-1233'},
   ]}
+  getId = () => {
+    // NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
+  addContact = (incomingContact) => {
+    const { contacts } = this.state
+    const newContact = { id: this.getId(), ...incomingContact }
+    this.setState({ contacts: [ newContact, ...contacts ]})
+  }
   render() {
     const { contacts } = this.state
     return(
       <div>
+        <ContactForm addContact={this.addContact} />
         <Header size="huge" color='blue' textAlign='center'>
           Contact list
         </Header>
---
+++ src/components/contacts/ContactForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Form } from 'semantic-ui-react';
+class ContactForm extends Component {
+  state = { firstName: '', phone: '' }
+  // change the state to be the user input
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+    // this.setState({ firstName: 'bob' })
+    // this.setState({ phone: '1123-123-1233' })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // add the contact to the array of obj
+    this.props.addContact(this.state)
+    // clear out the form
+    this.setState({ firstName: '', phone: '' })
+  }
+  render() {
+    const { firstName, phone } = this.state
+    return(
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Input
+          name='firstName'
+          value={firstName}
+          onChange={this.handleChange}
+          required
+          label='First Name'
+        />
+        <Form.Input
+          name='phone'
+          value={phone}
+          onChange={this.handleChange}
+          required
+          label='phone'
+        />
+        <Form.Button>Submit</Form.Button>
+      </Form>
+    )
+  }
+}
+export default ContactForm;
