84d7579ab4a931710a5dea759d8c474c0cdd8028
Logan Walker <loganwalker22@gmail.com>
2020-06-16 19:41:20 -0600

    contacts and contact components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
-
+import Contacts from './components/contacts/Contacts';
 class App extends Component {
   state = { contacts: [
     { id: 1, firstName: 'John', phone: '123-123-1233'},
     { id: 2, firstName: 'Sally', phone: '123-333-1233'},
     { id: 3, firstName: 'Alex', phone: '123-125-1233'},
   ]}
-
   render() {
+    const { contacts } = this.state
     return(
       <div>
         <Header size="huge" color='blue' textAlign='center'>
           Contact list
         </Header>
+        <Contacts contacts={contacts} />
       </div>
     )
   }
 }
-
 export default App;
